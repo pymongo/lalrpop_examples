a3a4e564706b18f2fd7c6de561152051f0561f01
wuaoxiang <wuaoxiang@stargraph.cn>
2021-09-22 09:36:58 +0800

    learn lalrpop <> expression

--- src/parsing_parenthesized_numbers/mod.rs
+++ src/parsing_parenthesized_numbers/mod.rs
@@ -5,4 +5,9 @@ fn feature() {
     let p = parser::NumParser::new();
     dbg!(p.parse("12313").unwrap());
     assert!(p.parse("rust").is_err());
+    
+    let p = parser::TermParser::new();
+    assert!(p.parse("123").is_ok());
+    assert!(p.parse("(123)").is_ok());
+    assert!(p.parse("((123))").is_ok());
 }
