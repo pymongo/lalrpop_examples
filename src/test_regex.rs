55c812284a527192f9a139e8e0bcbf665d411de9
wuaoxiang <wuaoxiang@stargraph.cn>
2021-09-23 11:01:37 +0800

    add Json::F64 and Json::Arr parser

--- src/test_regex.rs
+++ src/test_regex.rs
@@ -1,10 +1,9 @@
 fn test_regex(re: &str, text: &str) {
-    assert!(regex::Regex::new(re)
-        .unwrap()
-        .captures(text)
-        .unwrap()
-        .get(0)
-        .is_some());
+    let res = regex::Regex::new(re)
+    .unwrap()
+    .captures(text)
+    .unwrap().get(0).unwrap();
+    dbg!(res);
 }
 
 #[test]
@@ -22,4 +21,22 @@ fn test_num_parser() {
 
     test_regex(r"[0-9]+", "123");
     test_regex(r"[[0-9]]+", "123");
+}
+
+#[test]
+fn test_f64_parser() {
+    let re = r"[-+]?([0-9]*[.])?[0-9]+([eE][-+]?\d+)?";
+    test_regex(re, "1.6e-9");
+    test_regex(re, "1.3E8");
+    test_regex(re, "1.3");
+    test_regex(re, ".31");
+}
+
+#[test]
+fn test_string_parser() {
+    let re = r#"(?:[^"\\]|\\.)*"#;
+    test_regex(re, "apple");
+    test_regex(re, "😅");
+    test_regex(re, "\u{F602}");
+    test_regex(re, "hello \"name\"");
 }
